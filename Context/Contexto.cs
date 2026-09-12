b83a4f3d167d7e8c1cced4ef3d0482816946d1b7
DionicioPujols <dionicio_hernandez@ucne.edu.do>
2026-09-12 17:52:04 -0400

    Add project files.

---
+++ Context/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroLibro.Models;
+
+namespace RegistroLibro.Context
+{
+    public class Contexto  : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto>options) : base(options) { } 
+        public DbSet<Libros> Libros { get; set; }
+    }
+}
