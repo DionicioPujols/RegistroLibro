febd0c94fa63273f7afcb4c33b74b0788016a176
DionicioPujols <dionicio_hernandez@ucne.edu.do>
2026-09-18 09:05:57 -0400

    Creacion de la clase Estudiantes

---
+++ Models/Estudiates.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Resources;
+
+namespace RegistroLibro.Models
+{
+    public class Estudiates
+    {
+        [Key]
+        public int EstudiantesID { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string? nombre { get; set; }
+        [Required(ErrorMessage = "La direccion es obligatoria.")]
+        public string? Direccion { get; set; }
+
+        [Required(ErrorMessage = "El Email es obligatorio")]
+        [EmailAddress(ErrorMessage = "Debe contener un correo valido")]
+        public string? Correo { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimeinto es obligatorio")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimeinto { get; set; } = DateTime.Today
+
+    }
+}
