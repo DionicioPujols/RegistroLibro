292ca2a9a8b971616a8869e216dadd5499d690d8
DionicioPujols <dionicio_hernandez@ucne.edu.do>
2026-09-18 08:46:23 -0400

    New branch RegistroEstudiantes y configuración del .gitignore

---
+++ Models/Libros.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroLibro.Models;
+
+public class Libros
+{
+    [Key]
+    public int LibroId { get; set; }
+
+    [Required(ErrorMessage = "Necesitas agregar un Titulo Obligatorio")]
+    public String? Titulo { get; set; }
+    [Required(ErrorMessage = "Colocar Nombre del Autor")]
+    public String? Autor { get; set; }
+    [Required(ErrorMessage = "Debes colocar el año de publicacion")]
+    public int anoPublicacion { get; set; }
+
+}
